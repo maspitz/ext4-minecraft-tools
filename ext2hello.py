98076360852f2c1a77787b8f5934a06c9205d73d
Michael Spitznagel <exultavit@gmail.com>
2022-08-29 14:40:29 -0400

    Add superblock data member examples

--- ext2hello.py
+++ ext2hello.py
@@ -1,4 +1,5 @@
 from _ext2_cffi import ffi, lib
+"""Example usage and exploration area for ext2 CFFI."""
 
 lib.initialize_ext2_error_table()
 
@@ -12,4 +13,13 @@ if errval != 0:
     raise ValueError(ffi.string(lib.error_message(errval)).decode())
 else:
     print(f"Opened file: {filename}")
-    
+
+# superblock
+sb = filsys[0].super
+
+print(f"inode count: {sb.s_inodes_count}")
+print(f"block count: {sb.s_blocks_count}")
+print(f"first inode: {sb.s_first_ino}")
+print(f"{sb.s_free_blocks_count=}")
+print(f"{sb.s_free_blocks_hi=}")
+print(f"{lib.ext2fs_free_blocks_count(sb)=}")
